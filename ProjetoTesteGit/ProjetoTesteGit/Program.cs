fbe886c449e4ac4f0844b515ee108c5c675fa809
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2020-07-29 16:31:27 -0300

    Acrescentada a mensagem de despedida

--- ProjetoTesteGit/ProjetoTesteGit/Program.cs
+++ ProjetoTesteGit/ProjetoTesteGit/Program.cs
@@ -9,6 +9,7 @@ namespace ProjetoTesteGit
             Console.WriteLine("Hello World!");
             Console.WriteLine("How are you today?");
             Console.WriteLine("Have a good day!");
+            Console.WriteLine("Goodbye");
 
             Console.ReadLine();
         }
