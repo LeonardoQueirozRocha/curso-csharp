bcbcab9e043e8ba678f9ae5ff2e55e6c2aa8cbb5
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2020-07-30 19:09:01 -0300

    Exercios sobre composicao de entidades CSharp

---
+++ StringBuilderExemplo/StringBuilderExemplo/Entities/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace StringBuilderExemplo.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment() { }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
