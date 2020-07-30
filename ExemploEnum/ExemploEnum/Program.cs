077c31e950d9c821b344035ed6d5f0e0f9c240e9
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2020-07-29 23:57:38 -0300

    Exemplo de implementacao de lista e enums

---
+++ ExemploEnum/ExemploEnum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ExemploEnum.Entities;
+using ExemploEnum.Entities.Enums;
+
+namespace ExemploEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
