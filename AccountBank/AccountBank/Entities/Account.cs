3c9e343f692adbe7c79b96650f43fea39ecbecde
Leonardo Queiroz Rocha <47957548+LeonardoQueirozRocha@users.noreply.github.com>
2020-08-10 21:55:27 -0300

    Excercise about exceptions treatment

---
+++ AccountBank/AccountBank/Entities/Account.cs
@@ -0,0 +1,48 @@
+﻿using AccountBank.Entities.Exceptions;
+using System;
+using System.Globalization;
+
+namespace AccountBank.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Deposit(balance);
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if ((amount > WithdrawLimit && amount < Balance) || (amount > WithdrawLimit && amount > Balance))
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+            else if (amount < WithdrawLimit && amount > Balance)
+            {
+                throw new DomainException("Not enough balance");
+            }
+
+            Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return "New balance: " + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
