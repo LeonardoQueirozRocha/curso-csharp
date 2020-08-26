7146ed2d93455ed34adc1cb663b24ab0cce366fb
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2020-08-26 18:01:23 -0300

    Exercicio sobre restricoes de generics, modo simples

---
+++ ExemploRestricoesGererics/ExemploRestricoesGererics/Program.cs
@@ -0,0 +1,30 @@
+﻿using ExemploRestricoesGererics.Services;
+using System;
+using System.Collections.Generic;
+
+namespace ExemploRestricoesGererics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            int max = calculationService.Max(list);
+
+            Console.WriteLine("Max: ");
+            Console.WriteLine(max);
+        }
+    }
+}
