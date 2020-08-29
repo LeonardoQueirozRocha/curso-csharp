214566c684055f544e0960cb0434d4290b1301ab
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2020-08-29 18:26:26 -0300

    Exemples of how to use HashSet in Classes reference and Classes values

--- ExampleHashSetAndSortedSet/ExampleHashSetAndSortedSet/Program.cs
+++ ExampleHashSetAndSortedSet/ExampleHashSetAndSortedSet/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExampleHashSetAndSortedSet.Entities;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 
@@ -8,45 +9,20 @@ namespace ExampleHashSetAndSortedSet
     {
         static void Main(string[] args)
         {
-            //HashSet<string> set = new HashSet<string>();
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.00));
+            a.Add(new Product("Notebook", 1200.00));
 
-            //set.Add("TV");
-            //set.Add("Notebook");
-            //set.Add("Tablet");
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
 
-            //Console.WriteLine(set.Contains("Computer"));
+            Product prod = new Product("Notebook", 1200.00);
+            Console.WriteLine(a.Contains(prod));
 
-            //foreach (string p in set)
-            //{
-            //    Console.WriteLine(p);
-            //}
+            Point p = new Point(5, 10);
+            Console.WriteLine(b.Contains(p));
 
-            SortedSet<int> a = new SortedSet<int> { 0, 2, 4, 5, 6, 8, 10 };
-            SortedSet<int> b = new SortedSet<int> { 5, 6, 7, 8, 9, 10 };
-
-            // union
-            SortedSet<int> c = new SortedSet<int>(a);
-            c.UnionWith(b);
-            PrintCollection(c);
-
-            // intersection
-            SortedSet<int> d = new SortedSet<int>(a);
-            d.IntersectWith(b);
-            PrintCollection(d);
-
-            // difference
-            SortedSet<int> e = new SortedSet<int>(a);
-            e.ExceptWith(b);
-            PrintCollection(e);
-        }
-
-        static void PrintCollection<T>(IEnumerable<T> collection)
-        {
-            foreach (T obj in collection)
-            {
-                Console.Write(obj + " ");
-            }
-            Console.WriteLine();
         }
     }
 }
