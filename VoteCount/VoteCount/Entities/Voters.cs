b4124a855b12f6c16e9fbac1e8cb3577628b7bac
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2020-09-13 11:56:52 -0300

    Exemplo e exercicio sobre dictionary

---
+++ VoteCount/VoteCount/Entities/Voters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoteCount.Entities
+{
+    class Voters
+    {
+        public string Name { get; set; }
+        public int VoteQuantity { get; set; }
+
+        public Voters(string name, int voteQuantity)
+        {
+            Name = name;
+            VoteQuantity = voteQuantity;
+        }
+    }
+}
---
+++ VoteCount/VoteCount/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Xml;
+using VoteCount.Entities;
+
+namespace VoteCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> voters = new Dictionary<string, int>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int voteQuantity = int.Parse(line[1]);
+
+                        Voters voter = new Voters(name, voteQuantity);
+
+                        if (voters.ContainsKey(voter.Name))
+                        {
+                            voters[voter.Name] += voter.VoteQuantity;
+                        }
+                        else
+                        {
+                            voters.Add(voter.Name, voter.VoteQuantity);
+                        }
+                    }
+
+                    foreach (var item in voters)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
